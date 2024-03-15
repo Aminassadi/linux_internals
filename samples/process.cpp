49e7e3dd02f92a0fc1a80a904a8895a29cdf4a53
Aminassadi <aminassadi.og@gmail.com>
2024-03-15 05:20:48 -0400

    [REF] informations

--- samples/process.cpp
+++ samples/process.cpp
@@ -4,13 +4,36 @@
 #include <string.h>
 #include <sys/resource.h>
 
-class ProcessPri : public ::testing::Test 
+
+/*
+    nice() adds inc to the nice value for the calling thread.  (A
+    higher nice value means a lower priority.)
+
+    The range of the nice value is +19 (low priority) to -20 (high
+    priority).  Attempts to set a nice value outside the range are
+    clamped to the range.
+
+    Traditionally, only a privileged Process could lower the nice
+    value (i.e., set a higher priority).
+
+    A successful call can legitimately return -1 :))).  To detect an
+    error, set errno to 0 before the call, and check whether it is
+    nonzero after nice() returns -1.
+*/
+
+/*
+    from the kernel view there is no difference between Process and thread,
+    both name as task, threads are tasks with the same address space.
+*/
+
+
+class Process : public ::testing::Test 
 {   
 protected:
     static constexpr auto kDefaultNiceValue{0}; 
     static constexpr auto kMaxNiceValue{-20};
     static constexpr auto kMinNiceValue{19};
-    ProcessPri() 
+    Process() 
     {
         // This method will be called before each test.
         Reset();
@@ -38,30 +61,8 @@ protected:
     } 
 };
 
- /*
-    nice() adds inc to the nice value for the calling thread.  (A
-    higher nice value means a lower priority.)
-
-    The range of the nice value is +19 (low priority) to -20 (high
-    priority).  Attempts to set a nice value outside the range are
-    clamped to the range.
-
-    Traditionally, only a privileged ProcessPri could lower the nice
-    value (i.e., set a higher priority).
-
-    A successful call can legitimately return -1 :))).  To detect an
-    error, set errno to 0 before the call, and check whether it is
-    nonzero after nice() returns -1.
-*/
-
-/*
-    from the kernel view there is no difference between ProcessPri and thread,
-    both name as task, threads are tasks with the same address space.
-*/
-
-
 //setting the high priority nice value needs root privilege
-TEST_F(ProcessPri, set_priority_high)
+TEST_F(Process, set_priority_high)
 {   
     errno = 0;
     //increase the priority of the curent task  
@@ -72,7 +73,7 @@ TEST_F(ProcessPri, set_priority_high)
     EXPECT_EQ(getpriority(PRIO_PROCESS, 0), kMaxNiceValue);    
 }
 
-TEST_F(ProcessPri, set_priority_low)
+TEST_F(Process, set_priority_low)
 {
     errno = 0;
     //decrease the priority of the curent task   
