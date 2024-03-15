13f9ff87b1d1695d402c80d7b9040e4444f5dcc2
Aminassadi <aminassadi.og@gmail.com>
2024-03-15 07:20:57 -0400

    [ADD] real-time schedular policies test cases

--- samples/process.cpp
+++ samples/process.cpp
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <sys/resource.h>
 
-
+#define PRINT_MACRO_NAME(X) printf("%s", #X)
 /*
     nice() adds inc to the nice value for the calling thread.  (A
     higher nice value means a lower priority.)
@@ -27,8 +27,30 @@
 */
 
 
+
 class Process : public ::testing::Test 
-{   
+{ 
+private:
+    void ResetNiceValue()
+    {
+        errno = 0;
+        auto current_nice_value{getpriority(PRIO_PROCESS, 0)};        
+        ASSERT_FALSE(current_nice_value == -1);
+        std::cout<<"current nice value: " << current_nice_value << '\n';
+        if(nice(kDefaultNiceValue - current_nice_value) == -1)
+        {
+            EXPECT_FALSE( errno == -1) << "setting default nice value failed with error: "<<strerror(errno);
+        }
+        std::cout<<"nice value reset to it's default: " << getpriority(PRIO_PROCESS, 0) << '\n';
+    }
+
+    void ResetSchedPolicy()
+    {
+        auto min_sched_priority{sched_get_priority_min(SCHED_OTHER)};
+        const sched_param param{min_sched_priority};
+        //0 can be used instead of getpid(), measns calling process
+        ASSERT_EQ(sched_setscheduler(0, SCHED_OTHER, &param), 0) << "cannot set the schedular policy with error: " << strerror(errno) << '\n';
+    }
 protected:
     static constexpr auto kDefaultNiceValue{0}; 
     static constexpr auto kMaxNiceValue{-20};
@@ -39,7 +61,6 @@ protected:
         Reset();
     }
 
-
     void SetUp() override 
     {
     }
@@ -48,17 +69,33 @@ protected:
     {
     }
 
+    
+
     void Reset()
     {
-        errno = 0;
-        auto current_nice_value{getpriority(PRIO_PROCESS, 0)};
-        ASSERT_FALSE(current_nice_value == -1);
-        if(nice(kDefaultNiceValue - current_nice_value) == -1)
-        {
-            EXPECT_FALSE( errno == -1) << "setting default nice value failed with error: "<<strerror(errno);
-        }
-        std::cout<<"nice value reset to its default: " << getpriority(PRIO_PROCESS, 0) << '\n';
+        ResetNiceValue();
+        ResetSchedPolicy();
     } 
+
+
+    void PrintShcedularPolicyForCurrentProcess()
+    {
+        switch (sched_getscheduler(getpid()))
+        {
+        case SCHED_OTHER:
+            std::cout<<"normal process, round-robin schedular\n";
+            break;
+        case SCHED_FIFO:
+            std::cout<<"real-time process, FIFO schedular\n";
+            break;
+        case SCHED_RR:
+            std::cout<<"real-time process, round-robin schedular\n";
+            break;
+        default:
+            break;
+        }  
+    }
+
 };
 
 //setting the high priority nice value needs root privilege
@@ -83,6 +120,51 @@ TEST_F(Process, set_priority_low)
     } 
     EXPECT_EQ(getpriority(PRIO_PROCESS, 0), kMinNiceValue);    
 }
+
+
+/*
+    For processes scheduled under one of the normal scheduling policies (SCHED_OTHER, SCHED_IDLE, SCHED_BATCH), 
+    sched_priority is not used in scheduling decisions (it must be specified as 0). 
+    setting real-time scheduling policy needs root privileg.
+*/
+
+TEST_F(Process, sched_setscheduler_fifo_min)
+{
+    auto min_sched_priority{sched_get_priority_min(SCHED_FIFO)};
+    std::cout<<"mimimum priority for fifo schedular policy is " << min_sched_priority << '\n';
+    const sched_param param{min_sched_priority};
+    EXPECT_EQ(sched_setscheduler(0, SCHED_FIFO, &param), 0) << "cannot set the schedular policy with error: " << strerror(errno) << '\n';
+    PrintShcedularPolicyForCurrentProcess();
+}
+
+TEST_F(Process, sched_setscheduler_fifo_max)
+{
+    auto max_sched_priority{sched_get_priority_max(SCHED_FIFO)};
+    std::cout<<"maximum priority for fifo schedular policy is " << max_sched_priority << '\n';
+    const sched_param param{max_sched_priority};
+    EXPECT_EQ(sched_setscheduler(0, SCHED_FIFO, &param), 0) << "cannot set the schedular policy with error: " << strerror(errno) << '\n';
+    PrintShcedularPolicyForCurrentProcess();
+}
+
+
+TEST_F(Process, sched_setscheduler_rr_min)
+{
+    auto min_sched_priority{sched_get_priority_min(SCHED_RR)};
+    std::cout<<"mimimum priority for round-robin schedular policy is " << min_sched_priority << '\n';
+    const sched_param param{min_sched_priority};
+    EXPECT_EQ(sched_setscheduler(0, SCHED_FIFO, &param), 0) << "cannot set the schedular policy with error: " << strerror(errno) << '\n';
+    PrintShcedularPolicyForCurrentProcess();
+}
+
+TEST_F(Process, sched_setscheduler_rr_max)
+{
+    auto max_sched_priority{sched_get_priority_max(SCHED_RR)};
+    std::cout<<"maximum priority for round-robin schedular policy is " << max_sched_priority << '\n';
+    const sched_param param{max_sched_priority};
+    EXPECT_EQ(sched_setscheduler(0, SCHED_RR, &param), 0) << "cannot set the schedular policy with error: " << strerror(errno) << '\n';
+    PrintShcedularPolicyForCurrentProcess();
+}
+
 
 
 
